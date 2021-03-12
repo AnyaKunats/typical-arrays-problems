fe475ef5cbe4452d2969d810c3073f98418ad289
AnyaKunats <36382350+AnyaKunats@users.noreply.github.com>
2021-03-12 20:07:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 exports.min = function min (array) {
 
-  return array.length===0 ? 0 : array.reduce((a,b)=>Math.min(a,b));
+  return array.length===0 || !array ? 0 : array.reduce((a,b)=>Math.min(a,b));
 }
 
 exports.max = function max (array) {
-    return array.length===0 ? 0 : array.reduce((a,b)=>Math.max(a,b));
+    return array.length===0 || !array ? 0 : array.reduce((a,b)=>Math.max(a,b));
 }
 
 exports.avg = function avg (array) {
-  return array.length===0 ? 0 : array.reduce((a,b)=>a+b)/array.length;
+  return array.length===0 || !array ? 0 : array.reduce((a,b)=>a+b)/array.length;
 }
